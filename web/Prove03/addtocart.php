ef4b044d4b1a0e211dd183a744f80ae3a102b2f3
Freddy Kouakou <kou17001@byui.edu>
2019-01-26 23:17:06 +0000

    Prove03

---
+++ web/Prove03/addtocart.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+$fruits = $_POST['fruits'];
+foreach ($fruits as $addedFruits) {
+
+if (isset($_SESSION[$addedFruits])) {//Check the quantity of items in a session
+    $_SESSION[$addedFruits] += 1;
+}else {
+    $_SESSION[$addedFruits] = 1;
+}
+}
+
+$openCloseSession = $_SERVER['HTTP_REFERER'];
+header('Location: '. $openCloseSession);
+die();
+
+
+?>
---
+++ web/Prove03/shopping.php
@@ -0,0 +1,34 @@
+<?php session_start(); //Session gives access to the session array($_SESSION)
+$priceArray = array('Orange' => 1, 'Mango'=>2, 'Banana'=>3, 'Tangerine'=>2, 'Pineapple'=>5);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Shopping Cart</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Quantity</th>
+        </tr>
+        <?php
+        foreach ($_SESSION as $arrayIndex => $arrayValue) {
+            echo "<tr><td>$arrayIndex</td>";
+            echo "<td>$priceArray[$arrayIndex]</td>";
+            echo "<td>$arrayValue</td></tr>";
+        }
+        
+        ?>
+    </table>
+<br>
+<br>
+<ul>
+    <li><a href="browse.html">Return to Shopping</a></li>
+</ul>
+</body>
+</html>
