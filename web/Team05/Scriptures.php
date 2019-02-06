0f8a17084dc42290b796ad9f844eecaafb3f9a06
Freddy Kouakou <kou17001@byui.edu>
2019-02-06 22:46:29 +0000

    Committing from cmd

---
+++ web/Team05/Scriptures.php
@@ -0,0 +1,46 @@
+<?php
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Scriptures</title>
+</head>
+<body>
+    <h1>Scriptures Resources</h1>
+    <?php
+    foreach ($db->query('SELECT book, chapter, verse, content FROM Scriptures') as $row) {
+        echo 'b' .
+        $row['book'] . ' ' .
+        $row['chapter'] . ' : ' .
+        $row['verse'] . '</b>' . ' - '.
+        $row['content'] . '</br>';
+    }
+    ?>
+</body>
+</html>
+
