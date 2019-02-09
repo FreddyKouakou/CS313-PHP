8d6d591807cebd4be52379adb99a26daee6097e6
Freddy Kouakou <kou17001@byui.edu>
2019-02-09 00:35:04 +0000

    Committing to heroku

--- db/myDb.sql
+++ db/myDb.sql
@@ -1,41 +1,42 @@
 CREATE TABLE students(
     student_ID SERIAL PRIMARY KEY, 
-    first_name VARCHAR(40),
-    last_name VARCHAR(40)
+    first_name VARCHAR(50),
+    last_name VARCHAR(50)
 );
 
 CREATE TABLE subjects(
     subject_ID SERIAL PRIMARY KEY,
-    subject_name VARCHAR  (20)
+    subject_name VARCHAR  (40)
 );
 
 CREATE TABLE departments(
-    department_ID INT PRIMARY KEY,
-    department_name VARCHAR(20)
+    department_ID SERIAL PRIMARY KEY,
+    department_name VARCHAR(40)
 );
 
 CREATE TABLE schools(
-    school_ID INT PRIMARY KEY,
-    school_name VARCHAR(20)
+    school_ID SERIAL PRIMARY KEY,
+    school_name VARCHAR(40)
 );
 
 CREATE TABLE terms(
-    term_ID INT PRIMARY KEY,
-    term_name VARCHAR(20)
+    term_ID SERIAL PRIMARY KEY,
+    term_name VARCHAR(40)
 );
 
-CREATE TABLE exercises(
-    exercise_ID INT PRIMARY KEY,
-    exercise_name VARCHAR (20) 
-);
 CREATE TABLE parents(
-    parents_ID INT PRIMARY KEY,
-    parents_name VARCHAR(40)
+    parents_ID SERIAL PRIMARY KEY,
+    student_ID INT REFERENCES students(student_ID),
+    first_name VARCHAR(40),
+    last_name VARCHAR(40),
+    phone VARCHAR(15),
+    users_name VARCHAR(40),
+    user_password VARCHAR(40)
 );
 --The class table---
 CREATE TABLE classes(
-    classe_ID INT PRIMARY KEY,
-    classe_name VARCHAR(20),
+    classe_ID SERIAL PRIMARY KEY,
+    classe_name VARCHAR(40),
     department_ID INT REFERENCES departments(department_ID)
 );
 /*
@@ -43,7 +44,7 @@ CREATE TABLE classes(
  referencing tems, subjects, schools and classes 
 */
 CREATE TABLE classrooms(
-    classroom_ID INT PRIMARY KEY,
+    classroom_ID SERIAL PRIMARY KEY,
     term_ID INT REFERENCES terms(term_ID),
     subject_ID INT REFERENCES subjects(subject_ID),
     school_ID INT REFERENCES schools(school_ID),
@@ -52,9 +53,9 @@ CREATE TABLE classrooms(
 
 --The marks table---
 CREATE TABLE marks(
-mark_ID INT PRIMARY KEY,
+mark_ID SERIAL PRIMARY KEY,
 classroom_ID INT REFERENCES classrooms(classroom_ID),
 student_ID INT REFERENCES students(student_ID),
-exercise_ID INT REFERENCES exercises(exercise_ID),
+exercise_name VARCHAR(40),
 score INT
 );
