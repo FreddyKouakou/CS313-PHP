ba92aff2f65c6b48cdb693500960ad147441ef72
Freddy Kouakou <kou17001@byui.edu>
2019-02-17 03:41:13 +0000

    Insert

--- web/db/insert.sql
+++ web/db/insert.sql
@@ -1,6 +1,3 @@
-INSERT INTO students(classroom_id, first_name, last_name) VALUES(1,'Freddy', 'Kouakou');
-INSERT INTO students(classroom_id, first_name, last_name) VALUES(2,'Michael', 'Schmidt');
-INSERT INTO students(classroom_id, first_name, last_name) VALUES(3,'Paulie', 'Snyder');
 INSERT INTO subjects(subject_name) VALUES('French');
 
 INSERT INTO departments(department_name) VALUES('Nusery');
@@ -12,6 +9,10 @@ INSERT INTO classes(classe_name, department_ID) VALUES('class One', 1);
 INSERT INTO classrooms(term_ID, subject_ID, school_ID, classe_ID, classroom_name) VALUES(1, 1, 1, 1, 'Class One');
 INSERT INTO classrooms(term_ID, subject_ID, school_ID, classe_ID, classroom_name) VALUES(1, 1, 1, 1, 'Class Two');
 INSERT INTO classrooms(term_ID, subject_ID, school_ID, classe_ID, classroom_name) VALUES(1, 1, 1, 1, 'Class Three');
+INSERT INTO students(classroom_id, first_name, last_name) VALUES(1,'Freddy', 'Kouakou');
+INSERT INTO students(classroom_id, first_name, last_name) VALUES(2,'Michael', 'Schmidt');
+INSERT INTO students(classroom_id, first_name, last_name) VALUES(3,'Paulie', 'Snyder');
+
 INSERT INTO parents(student_ID, first_name, last_name, phone, users_name, user_password) VALUES(1, 'Michael', 'Schmidt', '+233000000000', 'Fred', 'Fred0011');
 
 INSERT INTO marks(student_ID, exercise_name, score) VALUES(1, 'Individual Class Test 30', 25);
