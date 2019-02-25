f4caebf924fc987ef98ce93124c4d0f78268a72d
Freddy Kouakou <kou17001@byui.edu>
2019-02-25 22:52:23 +0000

    insert last

--- web/db/insertteachers.php
+++ web/db/insertteachers.php
@@ -8,10 +8,10 @@ $getLastName = $_POST['last_name'];
 $getUserName = $_POST['users_name'];
 $getPhoneNumeber = $_POST['telephone'];
 $getClassroom_ID = $_POST['classroom'];
-echo $getClassroom_ID;
+// echo $getClassroom_ID;
 
 $db->query("INSERT INTO teachers(first_name, last_name, users_name, phone) VALUES('$getFirstName','$getLastName', '$getUserName', '$getPhoneNumeber')");
-$teachers_id = $db->lastInsertId("teacher_id");
+$teachers_id = $db->lastInsertId("teachers_teachers_ID_seq");
 $db->query("UPDATE classrooms SET teacher_id = $teachers_id WHERE classroom_id = $getClassroom_ID");
 
 header("location:teacherslogin.php");
