287f82f90292e8035a5ae2121f2ab0ae2a15dd69
Freddy Kouakou <kou17001@byui.edu>
2019-02-25 22:56:47 +0000

    teacher id

--- web/db/insertteachers.php
+++ web/db/insertteachers.php
@@ -11,7 +11,7 @@ $getClassroom_ID = $_POST['classroom'];
 // echo $getClassroom_ID;
 
 $db->query("INSERT INTO teachers(first_name, last_name, users_name, phone) VALUES('$getFirstName','$getLastName', '$getUserName', '$getPhoneNumeber')");
-$teachers_id = $db->lastInsertId("teachers_teachers_ID_seq");
+$teachers_id = $db->lastInsertId("teachers_teacher_ID_seq");
 $db->query("UPDATE classrooms SET teacher_id = $teachers_id WHERE classroom_id = $getClassroom_ID");
 
 header("location:teacherslogin.php");
